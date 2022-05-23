c8a24652833ebf905e069d9bd82efe9b214eb2a0
wth <return___0@163.com>
2022-05-23 17:14:35 +0800

    测试

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -28,7 +28,7 @@ const count = ref(0)
 
   <button type="button" @click="count++">count is: {{ count }}</button>
   <p>
-    Edit
+    Edit测试测试
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
 </template>
